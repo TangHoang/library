e1ee56d93526508648d0494f06f0b2496406bc06
Tang Hoang Griep <businesshoangtang@gmail.com>
2022-09-13 13:14:23 +0200

    add 'add books' feature

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const OnePiece = new Book("One Piece", "Oda", "true");
-let myLibrary = [OnePiece];
+let myLibrary = [];
+let isRead;
 const grid = document.querySelector(".grid-container");
 const form = document.querySelector(".form-container");
+const deleteButton = document.querySelector(".remove-button");
 const submitButton = document.getElementById("submit-button");
+const addButton = document.getElementById("add-button");
+const checkbox = document.querySelector("#reading-status");
+
+
+
+submitButton.onclick = () => { 
+    isRead = "not finished";
+    title = document.querySelector(".input-title").value;
+    author = document.querySelector(".input-author").value;    
+    addBookToLibrary(title, author, isRead);
+    form.style.display = "none";
+    form.classList.remove("overlay-shadow");
+}
 
 // constructor
 function Book(title, author, isRead){
@@ -15,6 +29,7 @@ function Book(title, author, isRead){
 function openForm(){
     form.style.display = "flex";
     form.classList.add("overlay-shadow");
+
 }
 
 function closeForm(){
@@ -24,12 +39,17 @@ function closeForm(){
 
 // get user input and create new book object to store in library
 function addBookToLibrary(title, author, status){
-    let newBook = new Book(title, author, status);
-    myLibrary.append(newBook);
+    const newBook = new Book(title, author, status);
+    myLibrary.push(newBook);
+    displayBooks(myLibrary.length-1);
 }
 
-function displayBooks(){
-    for(let i=0; i<myLibrary.length; i++){
+function clearGrid(){
+    grid.innerHTML = "";
+}
+
+function displayBooks(i){
+    for(i; i<myLibrary.length; i++){
         const bookCard = document.createElement("div");
         createCard(myLibrary[i], bookCard);
         bookCard.classList.add("book-card");
@@ -51,12 +71,29 @@ function createCard(book, bookCard){
     bookCard.appendChild(authorDiv);
 
     let isRead = book.isRead;
-    const isReadDiv = document.createElement("div");
-    isReadDiv.classList.add("isRead");
+    const isReadDiv = document.createElement("button");
+    isReadDiv.classList.add("status-button");
     isReadDiv.innerHTML = isRead;
+    isReadDiv.onclick = () => {
+        if(isReadDiv.innerHTML == "not finished"){
+            isReadDiv.innerHTML = "finished";
+            isReadDiv.classList.add("status-button-bg");
+        }
+        else if(isReadDiv.innerHTML == "finished"){
+            isReadDiv.innerHTML = "not finished";
+            isReadDiv.classList.remove("status-button-bg")
+        }
+    }
     bookCard.appendChild(isReadDiv);
 
     const removeButton = document.createElement("button");
+    // delete card feature
+    const currentBook = (element) => element == currentBook;
+    removeButton.onclick = () => {
+        removeButton.parentElement.remove();
+        const index = myLibrary.findIndex(currentBook);
+        myLibrary.splice(index, 1);
+    }
     removeButton.classList.add("remove-button");
     removeButton.innerHTML = "Delete";
     bookCard.appendChild(removeButton);
